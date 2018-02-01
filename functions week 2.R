599ff51fd1c042886c4fe7567801c5799492535f
Yuriy Mikheev <yuri.mikheev@gmail.com>
2018-02-01 19:23:15 +0300

    1

--- functions week 2.R
+++ functions week 2.R
@@ -1,18 +1,15 @@
-polutionmean <- function(directory, polutant, id = 1:332) {
+pollutantmean <- function(directory, polutant, id = 1:332) {
   monitorlist <- paste0(directory,"/",dir(directory)[id])
   
   tab <- data.frame()
-#  tempd = getwd()
-#  setwd(directory)
+
   for (monitor in monitorlist)
     tab <- rbind(tab,read.table(monitor, header = TRUE, sep = ","))
 
-#  setwd(tempd)
   mean(tab[[polutant]],na.rm = TRUE)
-  #tab
 }
 
-complite <- function(directory, id = 1:332) {
+complete <- function(directory, id = 1:332) {
   
   monitorlist <- paste0(directory,"/",dir(directory)[id])
   
@@ -25,4 +22,17 @@ complite <- function(directory, id = 1:332) {
 
   tr <- data.frame(id,v)
   tr
+}
+
+corr <- function(directory, threshold = 0) {
+  monitorlist <- paste0(directory,"/",dir(directory))
+  
+  v <- vector();
+  for (monitor in monitorlist) {
+    tab <- read.table(monitor, header = TRUE, sep = ",")
+    if (length(tab$sulfate[!is.na(tab$sulfate) & !is.na(tab$nitrate)]) > threshold)
+      v <- c(v,cor(tab$sulfate, tab$nitrate,  use = "complete.obs"))
+  }
+  
+  v
 }
